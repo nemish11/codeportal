39470a0a64da6fa1eb90845a421e6702faa1c89e
ckrokad <chintanrokad932@gmail.com>
2018-12-23 20:49:28 +0530

    css added

--- myproject/keepcodeapp/views.py
+++ myproject/keepcodeapp/views.py
@@ -38,10 +38,15 @@ def program_file(request):
 
 @login_required(login_url="/loginmodule/login")
 def upload_file(request):
+    c={}
     try:
         id = request.GET.get('id')
         username = request.session['username']
         file1 = request.FILES["codefile"]
+        if file1.size > 2*1024*1024 :
+            c["message_of_size"]="File too large. Size should not exceed 2 MB."
+            #return HttpResponseRedirect('/keepcodeapp')
+            return render(request,'index.html',c)
         fs = FileSystemStorage()
         filename = "./keepcodeapp/static/UserSubmissions/"+username+"_program"+str(id)+".c"
         if fs.exists(filename):
